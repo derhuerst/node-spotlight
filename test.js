f0348fe04ed7fbaa6ae180167810a15ac14bded0
Jannis R <mail@jannisr.de>
2017-01-11 21:13:40 +0100

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -2,18 +2,77 @@
 'use strict'
 
 const assert = require('assert')
+const timeout = require('p-timeout')
 const isStream = require('is-stream')
+const sink = require('stream-sink')
 
 const spotlight = require('.')
 
 const showError = (err) => {
-	console.error(err.stack)
+	console.error(err)
 	process.exit(1)
 	throw err
 }
 
+const isString = (s) => 'string' === typeof s
+const isNumber = (s) => 'number' === typeof s
+const isBoolean = (s) => 'boolean' === typeof s
+const isDate = (d) =>
+	d && d.toISOString && d.valueOf() === new Date(d.toISOString()).valueOf()
+const isArrayOf = (t) => (a) => Array.isArray(a) && a.every(t)
+const isArrayOfStrings = isArrayOf(isString)
+
+// We assume these attributes exist for every search result.
+const attributes = {
+	kMDItemContentType: isString,
+	kMDItemDateAdded: isDate,
+	kMDItemDisplayName: isString,
+	kMDItemFSInvisible: isBoolean,
+	kMDItemFSIsExtensionHidden: isBoolean,
+	kMDItemFSLabel: isNumber,
+	kMDItemFSName: isString,
+	kMDItemFSOwnerGroupID: isNumber,
+	kMDItemFSOwnerUserID: isNumber,
+	kMDItemFSSize: isNumber,
+	kMDItemFSTypeCode: isString,
+	kMDItemKind: isString,
+	kMDItemPhysicalSize: isNumber
+}
+
+
+
 console.info('platform:', process.platform)
 
+assert(isStream(spotlight('foo', __dirname)))
 
+timeout(
+	spotlight('sleep', '/bin')
+	.on('error', assert.ifError)
+	.pipe(sink('object'))
+, 1000)
+.then((results) => {
+	assert(results.length > 0)
+	for (let result of results) {
+		assert.deepStrictEqual(Object.keys(result), ['path'])
+		assert.strictEqual(typeof result.path, 'string')
+	}
 
-// todo
+	const sleep = results.find((result) => result.path === '/bin/sleep')
+	assert(sleep)
+})
+.catch(showError)
+
+timeout(
+	spotlight('sleep', '/bin', Object.keys(attributes))
+	.on('error', assert.ifError)
+	.pipe(sink('object'))
+, 1000)
+.then((results) => {
+	for (let result of results) {
+		for (let attr in attributes) {
+			const validator = attributes[attr]
+			assert(validator(result[attr]), `${attr} of ${result.path} invalid`)
+		}
+	}
+})
+.catch(showError)
